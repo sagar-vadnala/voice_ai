8a669cf3c5bc3643d9dce85837ded5f8301338c3
sagar-vadnala <sagaravadnala@gmail.com>
2023-12-13 18:27:56 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:voice_ai/feature_box.dart';
+import 'package:voice_ai/pallete.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final speechToText = SpeechToText();
+  String lastWords = "";
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() { });
+  }
+
+
+  /// Each time to start a speech recognition session
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    speechToText.stop();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Voice AI"),
+        leading: const Icon(Icons.menu),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //profile picture
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                        color: Pallete.assistantCircleColor,
+                        shape: BoxShape.circle),
+                  ),
+                ),
+                Container(
+                  height: 123,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage("assets/virtualAssistant.png"))),
+                ),
+              ],
+            ),
+            //chat bubble
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Pallete.borderColor,
+                  ),
+                  borderRadius: BorderRadius.circular(20).copyWith(
+                    topLeft: Radius.zero,
+                  )),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  "Good Morning, what task can I do for you? ",
+                  style: TextStyle(
+                      color: Pallete.mainFontColor,
+                      fontSize: 25,
+                      fontFamily: "Cera Pro"),
+                ),
+              ),
+            ),
+            //some text in below of the stack of the chat bubble
+            Container(
+              padding: const EdgeInsets.all(10),
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 10, left: 22),
+              child: const Text(
+                "Here are some features",
+                style: TextStyle(
+                  fontFamily: "Cera Pro",
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            //features list
+            const Column(
+              children: [
+                FeatureBox(
+                    color: Pallete.firstSuggestionBoxColor,
+                    headerText: "ChatGPT",
+                    description: 
+                      'A Smarter way to stay organized and informed with ChatGPT and new tech! ',
+                    ),
+                FeatureBox(
+                    color: Pallete.secondSuggestionBoxColor,
+                    headerText: "Dall-E",
+                    description: 
+                      'A Smarter way to stay organized and informed with ChatGPT and new tech! ',
+                    ),
+                FeatureBox(
+                    color: Pallete.thirdSuggestionBoxColor,
+                    headerText: "Smart Voice Assistant",
+                    description: 
+                      'A Smarter way to stay organized and informed with ChatGPT and new tech! ',
+                    ),
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: () async{
+          if(await speechToText.hasPermission &&
+          speechToText.isNotListening) {
+            await startListening();
+          } else if (speechToText.isListening) {
+            await stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
+        child: const Icon(Icons.mic),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:voice_ai/home_page.dart';
+import 'package:voice_ai/pallete.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Voice_ai',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor
+        )
+      ),
+      home: HomePage(),
+    );
+  }
+}
